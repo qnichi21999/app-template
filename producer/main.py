e3edc857aaf68a991f57524c00fdf58fff69b38d
almond <almondawwawa@gmail.com>
2025-04-14 14:35:50 +0300

    Add middleware

--- producer/main.py
+++ producer/main.py
@@ -1,12 +1,36 @@
 from fastapi import FastAPI
+from starlette.middleware.base import BaseHTTPMiddleware
 
 from producer.routers.auth import router as auth_router
 from producer.core.rabbitmq import rabbitmq_manager
+from producer.core.exceptions import ValidationError, UnauthorizedError
+from producer.core.security import verify_token
 
 app = FastAPI()
 
+class Middleware(BaseHTTPMiddleware):
+    async def dispatch(self, request, call_next):
+        public_paths = ["/docs", "/openapi.json", "/redoc", "/auth/login", "/auth/register"]
+
+        if any(request.url.path.startswith(path) for path in public_paths):
+            return await call_next(request)
+        
+        try:
+            auth_header = request.headers.get("Authorization")
+            if not auth_header or not auth_header.startswith("Bearer "):
+                raise UnauthorizedError("Missing or invalid Authorization header")
+            token = auth_header.split(" ")[1]
+            verify_token(token)
+            response = call_next(request)
+            return response
+        except Exception as e:
+            raise ValidationError
+
+#Routers
 app.include_router(auth_router)
 
+#Middleware
+app.add_middleware(Middleware)
 
 @app.on_event("startup")
 async def startup():
