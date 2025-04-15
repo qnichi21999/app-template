58b2a640c7d5cf001bffbdbaedc827d273169d59
qnichi21999 <21999german@gmail.com>
2025-04-15 13:26:07 +0300

    Change exceptions docstring to english

--- producer/core/exceptions.py
+++ producer/core/exceptions.py
@@ -1,12 +1,12 @@
 from fastapi import HTTPException, status
 
 class AppException(HTTPException):
-    """Базовый класс для всех исключений приложения"""
+    """Base class for all app's exceptions"""
     def __init__(self, status_code: int, detail: str):
         super().__init__(status_code=status_code, detail=detail)
 
 class UnauthorizedError(AppException):
-    """Ошибка авторизации"""
+    """Authorization error"""
     def __init__(self, detail: str = "Unauthorized"):
         super().__init__(
             status_code=status.HTTP_401_UNAUTHORIZED,
@@ -14,7 +14,7 @@ class UnauthorizedError(AppException):
         )
 
 class ForbiddenError(AppException):
-    """Ошибка доступа"""
+    """Access error"""
     def __init__(self, detail: str = "Forbidden"):
         super().__init__(
             status_code=status.HTTP_403_FORBIDDEN,
@@ -22,7 +22,7 @@ class ForbiddenError(AppException):
         )
 
 class NotFoundError(AppException):
-    """Ошибка - ресурс не найден"""
+    """Resource not found error"""
     def __init__(self, detail: str = "Not Found"):
         super().__init__(
             status_code=status.HTTP_404_NOT_FOUND,
@@ -30,7 +30,7 @@ class NotFoundError(AppException):
         )
 
 class ValidationError(AppException):
-    """Ошибка валидации"""
+    """Validation error"""
     def __init__(self, detail: str = "Validation Error"):
         super().__init__(
             status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
@@ -38,7 +38,7 @@ class ValidationError(AppException):
         )
 
 class InternalServerError(AppException):
-    """Внутренняя ошибка сервера"""
+    """Internal server error"""
     def __init__(self, detail: str = "Internal Server Error"):
         super().__init__(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
@@ -46,7 +46,7 @@ class InternalServerError(AppException):
         )
 
 class ServiceUnavailableError(AppException):
-    """Сервис недоступен"""
+    """Service unavailable"""
     def __init__(self, detail: str = "Service Unavailable"):
         super().__init__(
             status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
