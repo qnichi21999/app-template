c6046d74fb8e10b3df133e75aafb796ac5222bd6
almond <almondawwawa@gmail.com>
2025-04-16 01:42:34 +0300

    Add algorithm import from .env

--- producer/core/config.py
+++ producer/core/config.py
@@ -8,7 +8,7 @@ load_dotenv()
 class Settings(BaseSettings):
     # Security settings
     SECRET_KEY: str
-    ALGORITHM: str = "HS256"  # Default to HS256
+    ALGORITHM: str  # Default to HS256
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     REFRESH_TOKEN_EXPIRE_DAYS: int = 7
     
